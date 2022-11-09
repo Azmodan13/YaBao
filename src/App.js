ce162fb1949c2d4ea5771d43db0a0471981c82e6
Dima Taran <dima_taran@live.com>
2022-11-09 07:48:18 +0200

    create fetch slice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import Home from "./Pages/Home";
+import {useEffect} from 'react';
+import { useDispatch} from 'react-redux';
+import {fetchProduct} from './Redux/slice/productSlice'
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProduct());
+  }, [dispatch]);
+
   return (
     <Home/>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,13 @@ import Header from "../Components/Header/Header"
 import Nav from "../Components/Nav/Nav"
 import Footer from "../Components/Footer/Footer"
 
+
+
 export default function Home(){
+
+
+
+
 
     return(
         <>
